34081f53c657ec682609ef854ed2683d8b912694
ISA-KAYABASI <dinyester.975@gmail.com>
2023-09-03 16:36:53 +0300

    no message

--- backend/server.js
+++ backend/server.js
@@ -18,4 +18,4 @@ app.use('/api/goals', require('./routes/goalRoutes') ),
 
 app.use(errorHandler)
 
-app.listen(port, () => console.log('Server started on port '+ port))
+app.listen(port, () => console.log('Server started on port '+ port ))
